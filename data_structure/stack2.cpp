73c0c36203260ad3e6b1cb7a6d80d1870ca5e2e6
ji234 <tingjiang.gong@qq.com>
2022-12-17 10:46:48 +0800

    change:makefile and new stack2

---
+++ data_structure/stack2.cpp
@@ -0,0 +1,21 @@
+//反转一个字符串,
+//反转一个链表
+#include<iostream>
+using namespace std;
+
+class Stack{
+private:
+    char A[101];
+    int top;
+public:
+    void Push(int x);
+    void Pop();
+    int Top();
+    int isEmpty();
+};
+
+
+int main(){
+    
+    
+}
