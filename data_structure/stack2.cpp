b5d0bf035f0e7cf95febf3c3f80943db148cf185
gtj <2387322007@qq.com>
2022-12-18 13:45:36 +0800

    反转字符串被实现了

--- data_structure/stack2.cpp
+++ data_structure/stack2.cpp
@@ -1,21 +1,74 @@
-//反转一个字符串,
-//反转一个链表
+//反转一个字符串, 直接进入然后top pop出来就行
+//反转一个链表：要写一个逻辑一样的代码，视频里面使用C++标准库实现 使用了模板
+
+//在这里我只想实现反转字符串,链表不想实现想用模板实现
 #include<iostream>
+#include<cstring>
+#define ALENGTH 101
 using namespace std;
 
 class Stack{
-private:
-    char A[101];
-    int top;
-public:
-    void Push(int x);
-    void Pop();
-    int Top();
-    int isEmpty();
+	private:
+	    char A[ALENGTH];
+	    int top;
+	public:
+	    void Push(char num);
+	    void Pop();
+	    char Top();
+	    int isEmpty();
+		Stack();
 };
 
+Stack :: Stack(){
+	top = -1;
+	memset(A,0,sizeof(A));
+}
+
+void Stack::Push(char num){
+	if(top>ALENGTH-1){
+		printf("error: overflow");
+		return;
+	}
+	top++;
+	A[top] = num;
+}
+
+void Stack::Pop(){
+	if(top<=-1){
+		printf("error: overflow");
+		return;
+	}
+	top--;
+}
+
+char Stack::Top(){
+	if(top <= -1){
+		printf("error: overflow");
+		return '\0';
+	}
+	return A[top];
+}
+
+int Stack::isEmpty(){
+	return top == -1;
+}
+
+void reverse(char * str, int len){
+	Stack *stack = new Stack();
+	int i=0;
+	for(i=0 ;i<len ;i++){
+		stack->Push(str[i]);
+	}
+	for(i=0 ; !stack->isEmpty();i++){
+		str[i] = stack->Top();
+		stack->Pop();
+	}
+}
 
 int main(){
-    
-    
+    char A[ALENGTH];
+	printf("input a string:");
+    cin.get(A,ALENGTH);
+	reverse(A,strlen(A));
+	printf("%s\n",A);
 }
